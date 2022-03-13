75b0ce2dffba536a1217487fc4a4d5d0af0a0a16
Ecrb3 <73610715+Ecrb3@users.noreply.github.com>
2022-03-13 13:10:04 -0300

    Update analisaNoticia.js

--- src/services/analisaNoticia.js
+++ src/services/analisaNoticia.js
@@ -2,21 +2,26 @@
 
 async function analisaNoticia(url){
         console.log("analisando");
-        const flaskUrl = `http://dde4-34-68-188-182.ngrok.io${url}`
+        const flaskUrl = `http://03cf-34-68-188-182.ngrok.io/${url}`
         const dados = await fetch(flaskUrl);
         const json = await dados.json();
         var labelNoticia = document.createElement('img');
+        var probNoticia = document.createElement('p')
+        probNoticia.innerHTML = json[1];
+        probNoticia
         //labelNoticia.innerHTML = json;
-        var menu = document.getElementsByTagName('p')[0];
         labelNoticia.style = 'display: inline-block; margin-left: 5px;'
+        probNoticia.style = 'display: inline-block; margin-left: 5px;'
+        var menu = document.getElementsByTagName('p')[0];
         console.log(json);
-        if(json == "[1.]"){
+        if(json[2] == "[1.]"){
                 labelNoticia.src="../../true.png";
         }else{
                 labelNoticia.src="../../false.png";
         }
         var menu = document.getElementsByTagName('p')[0];
         menu.appendChild(labelNoticia);
+        menu.appendChild(probNoticia);
         
           
         
